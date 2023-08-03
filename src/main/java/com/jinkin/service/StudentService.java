c31b03ad00308a74f2a6223fa59c00101a0c83b0
Saurabh Maithani <saurabhmaithani01921@gmail.com>
2023-08-03 16:26:03 +0530

    Update JinkinsControllerTest.java
    remove test case

---
+++ src/main/java/com/jinkin/service/StudentService.java
@@ -0,0 +1,44 @@
+package com.jinkin.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jinkin.entity.Student;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    private List<Student> students = new ArrayList<>();
+
+    public StudentService() {
+        // Initialize with some sample data
+        students.add(new Student(1L, "John Doe", 20));
+        students.add(new Student(2L, "Jane Smith", 22));
+        students.add(new Student(3L, "Michael Johnson", 19));
+    }
+
+    public List<Student> getAllStudents() {
+        return students;
+    }
+
+    public Student getStudentById(Long id) {
+        return students.stream()
+                .filter(student -> student.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Student addStudent(Student student) {
+        student.setId(generateNewId());
+        students.add(student);
+        return student;
+    }
+
+    private Long generateNewId() {
+        return students.stream()
+                .mapToLong(Student::getId)
+                .max()
+                .orElse(0L) + 1L;
+    }
+}
